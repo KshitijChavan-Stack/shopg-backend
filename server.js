6fd63fba0d5547559c56f9044720af6830b239f2
KshitijChavan-Stack <chavankshitij0@gmail.com>
2026-04-06 15:06:04 +0530

    fix: relax CORS for cross-domain Vercel frontend

--- server.js
+++ server.js
@@ -20,13 +20,7 @@ const allowedOrigins = [
 ].filter(Boolean);
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
